4549079c9e0a366f1120a26435c1a7801202da52
NearValentine <53266906+NearValentine@users.noreply.github.com>
2023-04-18 14:54:55 +0200

    Update skrypt.ps1

--- skrypt.ps1
+++ skrypt.ps1
@@ -1,93 +1,92 @@
-# AZ1_Projekt
-
-$csv = "c:\...\uzytkownicy.csv"
+# Set variables.
+$csv = "c:\...\users.csv"
 $domain = Get-ADDomain
 $folder = "c:\...\"
 $count = 0
 
-# Generator hasel
+# Password generator.
 $newPass = ''
 1..12 | ForEach-Object {$newPass += [char](Get-Random -Minimum 48 -Maximum 122)}
 
-# Funkcja tworzaca lokalizacje, w ktorej beda znajdowac sie wszystkie pliki wynikowe
+# Create destination folder.
 function folder
 {
     if(Test-Path $folder){}
     else{New-Item -Path $folder -ItemType Directory -Force}
 }
 
-# Funkcja menu glownego, sluzy do wybrania pozadanego dzialania
+# Main menu.
 function menu
 {
  cls
- $opcja = Read-Host "Prosze wybrac opcje:`n 1 – Obsluga kont uzytkownikow`n 2 - Obsluga kont grup`n 3 - Raporty`n q - Wyjdz"`n
- if($opcja -eq 1){a}
- elseif($opcja -eq 2){b}
- elseif($opcja -eq 3){c}
- elseif($opcja -eq 'q'){break}
+ $option = Read-Host "Select option:`n 1 – User accounts`n 2 - Group accounts`n 3 - Reports`n q - Exit"`n
+ if($option -eq 1){a}
+ elseif($option -eq 2){b}
+ elseif($option -eq 3){c}
+ elseif($option -eq 'q'){break}
   else
     {
-     Write-Host "Blad - Wybierz poprawna opcje" -ForegroundColor Red
+     Write-Host "Error - select correct option" -ForegroundColor Red
      Sleep 2
      menu
     }
 }
 
-# Podmenu do obslugi kont uzytkownikow
+# User accounts submenu.
 function a
 {
  cls
- $opcja = Read-Host "Prosze wybrac opcje:`n 1 – Tworzenie konta uzytkownika`n 2 - Tworzenie wielu kont na podstawie pliku csv`n 3 - Blokowanie konta uzytkownika`n 4 - Zmiana hasla konta uzytkownika`n q - Wroc"`n
- if($opcja -eq 1){tworzenie_konta}
- elseif($opcja -eq 2){csv}
- elseif($opcja -eq 3){blokada_konta}
- elseif($opcja -eq 4){zmiana_hasla}
-  elseif($opcja -eq 'q'){menu}
+ $option = Read-Host "Select option:`n 1 – Create user account`n 2 - Import accounts from csv file`n 3 - Block user account`n 4 - Change user account password`n q - Back"`n
+ if($option -eq 1){create_account}
+ elseif($option -eq 2){csv}
+ elseif($option -eq 3){block_account}
+ elseif($option -eq 4){change_password}
+  elseif($option -eq 'q'){menu}
  else
     {
-     Write-Host "Blad - Wybierz poprawna opcje" -ForegroundColor Red
+     Write-Host "Error - select correct option" -ForegroundColor Red
      Sleep 2
      a
     }
 }
 
-# Podmenu do obslugi kont grup
+# Group accounts submenu.
 function b
 {
  cls
- $opcja = Read-Host "Prosze wybrac opcje:`n 1 – Tworzenie nowych grup`n 2 - Dodawanie uzytkownikow do grup`n q - Wroc"`n
- if($opcja -eq 1){tworzenie_grupy}
- elseif($opcja -eq 2){dodanie_do_grupy}
-  elseif($opcja -eq 'q'){menu}
+ $option = Read-Host "Select option:`n 1 – Create new group`n 2 - Add user to group`n q - Back"`n
+ if($option -eq 1){create_group}
+ elseif($option -eq 2){add_to_group}
+  elseif($option -eq 'q'){menu}
  else
     {
-     Write-Host "Blad - Wybierz poprawna opcje" -ForegroundColor Red
+     Write-Host "Error - select correct option" -ForegroundColor Red
      Sleep 2
      b
     }
 }
 
-# Podmenu do raportow
+# Reports submenu.
 function c
 {
  cls
- $opcja = Read-Host "Prosze wybrac opcje:`n 1 – Lista grup z czlonkami`n 2 - Lista zablokowanych kont w domenie`n 3 - Lista szczegolowych informacji o kontach uzytkownikow`n 4 - Lista szczegolowych informacji o kontach komputerow w domenie`n 5 - Lista jednostek organizacyjnych w domenie`n q - Wrocæ"`n
- if($opcja -eq 1){lista_grup}
- elseif($opcja -eq 2){lista_kont_blok}
- elseif($opcja -eq 3){lista_konta}
- elseif($opcja -eq 4){lista_komp}
- elseif($opcja -eq 5){lista_ou}
- elseif($opcja -eq 'q'){menu}
+ $option = Read-Host "Select option:`n 1 – List group with members`n 2 - List blocked accounts`n 3 - List user accounts details`n 4 - List domain computers details`n 5 - List organizational units`n q - Back"`n
+ if($option -eq 1){group_list}
+ elseif($option -eq 2){list_blocked}
+ elseif($option -eq 3){list_accounts}
+ elseif($option -eq 4){list_computers}
+ elseif($option -eq 5){list_ou}
+ elseif($option -eq 'q'){menu}
  else
     {
-     Write-Host "Blad - Wybierz poprawna opcje" -ForegroundColor Red
+     Write-Host "Error - select correct option" -ForegroundColor Red
      Sleep 2
      c
     }
 }
 
-# Funkcja tworzaca konto uzytkownika
-function tworzenie_konta
+# Create user account.
+function create_account
 {
     [CmdletBinding()]
     param(
@@ -121,21 +120,21 @@ function tworzenie_konta
     a
 }
 
-# Funkcja tworzaca wiele kont na podstawie pliku csv
-function tworzenie_konta_csv
+# Import from accounts from csv.
+function create_account_csv
 {
     $users = Import-Csv $csv
     Foreach ($user in $users)
     {  
         $dane = [pscustomobject]@{
-                    Name = $user.imie + " " + $user.nazwisko
-                    GivenName = $user.imie
-                    Surname = $user.nazwisko
-                    Department = $user.dzial
-                    UserPrincipalName = $user.imie + "." + $user.nazwisko + "@" + $domain.DNSRoot
-                    SamAccountName = $user.imie + "." + $user.nazwisko
-                    DisplayName = $user.imie + " " + $user.nazwisko
-                    EmailAddress = $user.imie + "." + $user.nazwisko + "@" + $domain.DNSRoot
+                    Name = $user.name + " " + $user.surname
+                    GivenName = $user.name
+                    Surname = $user.surname
+                    Department = $user.department
+                    UserPrincipalName = $user.name + "." + $user.surname + "@" + $domain.DNSRoot
+                    SamAccountName = $user.name + "." + $user.surname
+                    DisplayName = $user.name + " " + $user.surname
+                    EmailAddress = $user.name + "." + $user.surname + "@" + $domain.DNSRoot
                     AccountPassword = ConvertTo-SecureString $newPass -AsPlainText -Force
                     Enabled = $true
                  }
@@ -159,150 +158,150 @@ function tworzenie_konta_csv
     a
 }
 
-# Funkcja generujaca pusty plik csv
+# Generate empty csv file.
 function csv
 {
     cls
-    $opcja = Read-Host "Proszê wybraæ opcje:`n 1 - Tworzenie wielu kont na podstawie pliku csv`n 2 - Generuj pusty csv`n q - Wróæ"`n
-    if($opcja -eq 1){tworzenie_konta_csv}
-    elseif($opcja -eq 2){
-                            $header = "imie","nazwisko","dzial" | Select-Object imie,nazwisko,dzial | Export-Csv -Path $csv -NoClobber -NoTypeInformation
+    $option = Read-Host "Select option:`n 1 - Import accounts from csv`n 2 - Generate empty csv`n q - Back"`n
+    if($option -eq 1){create_account_csv}
+    elseif($option -eq 2){
+                            $header = "name","surname","department" | Select-Object name,surname,department | Export-Csv -Path $csv -NoClobber -NoTypeInformation
                             a
                         }
-    elseif($opcja -eq 'q'){a}
+    elseif($option -eq 'q'){a}
     else
     {
-        Write-Host "B³¹d - Wybierz poprawn¹ opcjê" -ForegroundColor Red
+        Write-Host "Error - select correct option" -ForegroundColor Red
         Sleep 2
         a
     }
 }
 
-# Funkcja blokujaca konto uzytkownika
-function blokada_konta
+# Block user account.
+function block_account
 {
     cls
-    $block = Read-Host "Proszê wpisaæ login do zablokowania: "`n
+    $block = Read-Host "Login: "`n
     Get-ADUser -Filter 'SamAccountName -like $block' | Disable-ADAccount
-    $nazwa = Get-ADUser -Filter 'SamAccountName -like $block' | Select-Object Name
+    $login_name = Get-ADUser -Filter 'SamAccountName -like $block' | Select-Object Name
     $who = Get-ADUser $env:USERNAME
     $date = Get-Date -Format "MM/dd/yyyy HH:mm"
-    $log = "$($who.Name);$($nazwa.Name);$date" | Out-File -FilePath $folder"zablokowane_konta.txt" -Append
+    $log = "$($who.Name);$($login_name.Name);$date" | Out-File -FilePath $folder"blocked_accounts.txt" -Append
     a
 }
 
-# Funkcja zmieniajaca haslo uzytkownika
-function zmiana_hasla
+# Change password.
+function change_password
 {
     cls
-    $konto = Read-Host "Proszê wpisaæ login do zmiany has³a: "`n
-    $haslo = Read-Host "Prosze wpisaæ nowe has³o: "`n
-    $konto1 = Get-ADUser -Filter 'SamAccountName -like $konto'
-    Set-ADAccountPassword -Identity $konto1 -NewPassword (ConvertTo-SecureString -AsPlainText $haslo -Force)
-    $nazwa = Get-ADUser -Filter 'SamAccountName -like $konto' | Select-Object Name
+    $account = Read-Host "Login: "`n
+    $password = Read-Host "New password: "`n
+    $account1 = Get-ADUser -Filter 'SamAccountName -like $account'
+    Set-ADAccountPassword -Identity $account1 -NewPassword (ConvertTo-SecureString -AsPlainText $password -Force)
+    $login_name = Get-ADUser -Filter 'SamAccountName -like $account' | Select-Object Name
     $who = Get-ADUser $env:USERNAME
     $date = Get-Date -Format "MM/dd/yyyy HH:mm"
-    $log = "$($who.Name);$($nazwa.Name);$date" | Out-File -FilePath $folder"zmiana_hasla.txt" -Append
+    $log = "$($who.Name);$($login_name.Name);$date" | Out-File -FilePath $folder"change_password.txt" -Append
     a
 }
 
-# Funkcja tworzaca konto grupy
-function tworzenie_grupy
+# Create group account.
+function create_group
 {
     cls
-    $grupa = Read-Host "Proszê wpisaæ nazwê grupy: "`n
-    $check = Get-ADGroup $grupa
+    $group = Read-Host "Group name: "`n
+    $check = Get-ADGroup $group
     if($check)
     {
-        Write-Host "ta grupa juz istnieje" -ForegroundColor Green
+        Write-Host "This group alredy exists." -ForegroundColor Green
         Sleep 2
         b
     }
     else
     {
-        New-ADGroup -Name:$grupa -SamAccountName:$grupa -GroupScope:"Global"
+        New-ADGroup -Name:$group -SamAccountName:$group -GroupScope:"Global"
         $who = Get-ADUser $env:USERNAME 
         $date = Get-Date -Format "MM/dd/yyyy HH:mm"
         $log = "Creator Group Date" | ConvertFrom-String | Export-Csv -Path $folder"create_group.csv" -NoClobber -NoTypeInformation
-        $log = "$($who.Name);$grupa;$date" | ConvertFrom-String -Delimiter ";" | Export-Csv -Path $folder"create_group.csv" -NoClobber -NoTypeInformation -Append
+        $log = "$($who.Name);$group;$date" | ConvertFrom-String -Delimiter ";" | Export-Csv -Path $folder"create_group.csv" -NoClobber -NoTypeInformation -Append
     }
     b
 }
 
-# Funkcja dodajaca konto uzytkownika do grupy
-function dodanie_do_grupy
+# Add user to group.
+function add_to_group
 {
     cls
-    $grupa = Read-Host "Proszê wpisaæ nazwê grupy: "`n
-    $check = Get-ADGroup $grupa
+    $group = Read-Host "Group name: "`n
+    $check = Get-ADGroup $group
     if(!$check)
     {
-        Write-Host "ta grupa nie istnieje" -ForegroundColor Red
+        Write-Host "This group alredy exists." -ForegroundColor Red
         Sleep 2
         b
     }
     else
     {
-        $user = Read-Host "Proszê wpisaæ login u¿ytkownika: "`n
+        $user = Read-Host "Login: "`n
         $checkuser = Get-ADUser -Filter 'SamAccountName -like $user'
         if(!$checkuser)
         {
-            Write-Host "ten u¿ytkownik nie istnieje" -ForegroundColor Red
+            Write-Host "This user alredy exists." -ForegroundColor Red
             Sleep 2
             b
         }
         else
         {
-            Add-ADGroupMember -Identity $grupa -Members $user
+            Add-ADGroupMember -Identity $group -Members $user
             $who = Get-ADUser $env:USERNAME
-            $log = "$($who.Name);$user;$grupa" | Out-File -FilePath $folder"zmiana_czlonkostwa_grup.txt" -Append
+            $log = "$($who.Name);$user;$group" | Out-File -FilePath $folder"group_membership.txt" -Append
         }
     }
     b
 }
 
-# Funkcja generujaca raport z lista grup z czlonkami
-function lista_grup
+# Group list report.
+function group_list
 {
     Get-ADGroup -Filter * | Select-Object Name | % {
-    $grupa = $_
-    New-Item -Path $folder"$($grupa.Name).txt"
-    Get-ADGroupMember $grupa.Name | Select-Object SamAccountName | Out-File -FilePath $folder"$($grupa.Name).txt"
+    $group = $_
+    New-Item -Path $folder"$($group.Name).txt"
+    Get-ADGroupMember $group.Name | Select-Object SamAccountName | Out-File -FilePath $folder"$($group.Name).txt"
     }
     c
 }
 
-# Funkcja generujaca raport z lista zablokowanych kont
-function lista_kont_blok
+# Blocked list report.
+function list_blocked
 {
-    Get-ADUser -Filter {Enabled -eq $false} -properties WhenChanged | Select Name,DistinguishedName,SID,WhenChanged | Export-Csv -Path $folder"zablokowane_konta.csv" -NoClobber -NoTypeInformation
+    Get-ADUser -Filter {Enabled -eq $false} -properties WhenChanged | Select Name,DistinguishedName,SID,WhenChanged | Export-Csv -Path $folder"blocked_accounts.csv" -NoClobber -NoTypeInformation
     c
 }
 
-# Funkcja generujaca raport z lista uzytkownikow
-function lista_konta
+# Accounts list report.
+function list_accounts
 {
-    Get-ADUser -Filter * -Properties whenCreated,whenChanged,LastLogonDate,PasswordLastSet | Select GivenName,Surname,UserPrincipalName,SamAccountName,DistinguishedName,whenCreated,whenChanged,LastLogonDate,PasswordLastSet | Export-Csv -Path $folder"u¿ytkownicy.csv" -NoClobber -NoTypeInformation
+    Get-ADUser -Filter * -Properties whenCreated,whenChanged,LastLogonDate,PasswordLastSet | Select GivenName,Surname,UserPrincipalName,SamAccountName,DistinguishedName,whenCreated,whenChanged,LastLogonDate,PasswordLastSet | Export-Csv -Path $folder"users.csv" -NoClobber -NoTypeInformation
     c
 }
 
-# Funkcja generujaca raport z lista komputerow
-function lista_komp
+# Computers list report.
+function list_compusters
 {
     Get-ADComputer -Filter * -Properties Enabled,PasswordLastSet,whenCreated,OperatingSystem | % {
-    $komputery = $_
-    $komputery | Select Name,SID,DistinguishedName,Enabled,PasswordLastSet,whenCreated | Export-Csv -Path $folder"$($komputery.DNSHostName)_$($komputery.OpeatingSystem).csv" -NoClobber -NoTypeInformation
+    $computers = $_
+    $computers | Select Name,SID,DistinguishedName,Enabled,PasswordLastSet,whenCreated | Export-Csv -Path $folder"$($computers.DNSHostName)_$($computers.OpeatingSystem).csv" -NoClobber -NoTypeInformation
     }
     c
 }
 
-# Funkcja generujaca raport z lista jednostek organizacyjnych w domenie
-function lista_ou
+# Organizational Unit list report.
+function list_ou
 {
-    Get-ADOrganizationalUnit -Filter * -Properties Name,DistinguishedName | Select Name,DistinguishedName | Export-Csv -Path $folder"OS.csv" -NoClobber -NoTypeInformation
+    Get-ADOrganizationalUnit -Filter * -Properties Name,DistinguishedName | Select Name,DistinguishedName | Export-Csv -Path $folder"OU.csv" -NoClobber -NoTypeInformation
     c
 }
 
-cls # Wyczyszczenie konsoli
-folder # Utworzenie miejsca na wszystkie pliki i raporty
-menu # Wywolanie glownej funkcji
+cls # Clear console.
+folder # Create destination folder.
+menu # Run menu.
